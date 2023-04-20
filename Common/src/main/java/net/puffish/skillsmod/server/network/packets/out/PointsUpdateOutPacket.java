7d7d72be1b2cf3e8243d95c8cb9f11e2939eafc4
Pufferfish <puffish@puffish.net>
2023-04-20 17:59:38 +0200

    Cleanup packets

--- Common/src/main/java/net/puffish/skillsmod/server/network/packets/out/PointsUpdateOutPacket.java
+++ Common/src/main/java/net/puffish/skillsmod/server/network/packets/out/PointsUpdateOutPacket.java
@@ -1,11 +1,18 @@
 package net.puffish.skillsmod.server.network.packets.out;
 
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 import net.puffish.skillsmod.network.OutPacket;
 import net.puffish.skillsmod.network.Packets;
 
 public class PointsUpdateOutPacket extends OutPacket {
-	public PointsUpdateOutPacket(String categoryId, int points) {
+	public static PointsUpdateOutPacket write(String categoryId, int points) {
+		var packet = new PointsUpdateOutPacket();
+		write(packet.buf, categoryId, points);
+		return packet;
+	}
+
+	public static void write(PacketByteBuf buf, String categoryId, int points) {
 		buf.writeString(categoryId);
 		buf.writeInt(points);
 	}
