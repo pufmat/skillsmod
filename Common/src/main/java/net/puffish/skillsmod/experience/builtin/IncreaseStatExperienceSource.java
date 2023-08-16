493f887fe6a4a9bedbb263ce3257cee6162933d7
Pufferfish <puffish@puffish.net>
2023-08-16 22:32:48 +0200

    Add increase stat experience source

---
+++ Common/src/main/java/net/puffish/skillsmod/experience/builtin/IncreaseStatExperienceSource.java
@@ -0,0 +1,61 @@
+package net.puffish.skillsmod.experience.builtin;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stat;
+import net.minecraft.util.Identifier;
+import net.puffish.skillsmod.SkillsAPI;
+import net.puffish.skillsmod.SkillsMod;
+import net.puffish.skillsmod.config.ConfigContext;
+import net.puffish.skillsmod.experience.ExperienceSource;
+import net.puffish.skillsmod.experience.calculation.CalculationManager;
+import net.puffish.skillsmod.experience.calculation.condition.ConditionFactory;
+import net.puffish.skillsmod.experience.calculation.condition.StatCondition;
+import net.puffish.skillsmod.experience.calculation.parameter.EffectParameter;
+import net.puffish.skillsmod.experience.calculation.parameter.ParameterFactory;
+import net.puffish.skillsmod.json.JsonObjectWrapper;
+import net.puffish.skillsmod.utils.Result;
+import net.puffish.skillsmod.utils.failure.Failure;
+
+import java.util.Map;
+
+public class IncreaseStatExperienceSource implements ExperienceSource {
+	public static final Identifier ID = SkillsMod.createIdentifier("increase_stat");
+
+	private static final Map<String, ConditionFactory<Context>> CONDITIONS = Map.ofEntries(
+			Map.entry("stat", StatCondition.factory().map(p -> p.map(Context::stat)))
+	);
+
+	private static final Map<String, ParameterFactory<Context>> PARAMETERS = Map.ofEntries(
+			Map.entry("player_effect", EffectParameter.factory().map(p -> p.map(Context::player))),
+			Map.entry("amount", ParameterFactory.simple(ctx -> (double) ctx.amount()))
+	);
+
+	private final CalculationManager<Context> manager;
+
+	private IncreaseStatExperienceSource(CalculationManager<Context> calculated) {
+		this.manager = calculated;
+	}
+
+	public static void register() {
+		SkillsAPI.registerExperienceSourceWithData(
+				ID,
+				(json, context) -> json.getAsObject().andThen(rootObjet -> IncreaseStatExperienceSource.create(rootObjet, context))
+		);
+	}
+
+	private static Result<IncreaseStatExperienceSource, Failure> create(JsonObjectWrapper rootObject, ConfigContext context) {
+		return CalculationManager.create(rootObject, CONDITIONS, PARAMETERS, context).mapSuccess(IncreaseStatExperienceSource::new);
+	}
+
+	private record Context(ServerPlayerEntity player, Stat<?> stat, int amount) { }
+
+	public int getValue(ServerPlayerEntity player, Stat<?> stat, int amount) {
+		return manager.getValue(new Context(player, stat, amount));
+	}
+
+	@Override
+	public void dispose(MinecraftServer server) {
+
+	}
+}
--- Common/src/main/java/net/puffish/skillsmod/mixin/ServerPlayerEntityMixin.java
+++ Common/src/main/java/net/puffish/skillsmod/mixin/ServerPlayerEntityMixin.java
@@ -1,7 +1,9 @@
 package net.puffish.skillsmod.mixin;
 
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stat;
 import net.puffish.skillsmod.SkillsAPI;
+import net.puffish.skillsmod.experience.builtin.IncreaseStatExperienceSource;
 import net.puffish.skillsmod.rewards.builtin.AttributeReward;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,5 +15,16 @@ public abstract class ServerPlayerEntityMixin {
 	@Inject(method = "<init>", at = @At("RETURN"))
 	private void injectAtInit(CallbackInfo ci) {
 		SkillsAPI.refreshReward((ServerPlayerEntity) (Object) this, AttributeReward.ID);
+	}
+
+	@Inject(method = "increaseStat", at = @At("HEAD"))
+	private void injectAtIncreaseStat(Stat<?> stat, int amount, CallbackInfo ci) {
+		var player = (ServerPlayerEntity) (Object) this;
+		SkillsAPI.visitExperienceSources(player, experienceSource -> {
+			if (experienceSource instanceof IncreaseStatExperienceSource increaseStatExperienceSource) {
+				return increaseStatExperienceSource.getValue(player, stat, amount);
+			}
+			return 0;
+		});
 	}
 }
