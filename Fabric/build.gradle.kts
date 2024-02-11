d9fdea2ca412ac9bc0fa2a86ed46c929603544ee
Pufferfish <puffish@puffish.net>
2024-02-11 14:50:28 +0100

    Update Gradle

--- Fabric/build.gradle.kts
+++ Fabric/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     minecraft("com.mojang:minecraft:${project.properties["minecraft_version"]}")
     mappings("net.fabricmc:yarn:${project.properties["yarn_mappings"]}:v2")
 
-    modImplementation("net.fabricmc:fabric-loader:${project.properties["loader_version"]}")
+    modImplementation("net.fabricmc:fabric-loader:${project.properties["fabric_loader_version"]}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_api_version"]}")
 
     implementation(project(path = ":Common", configuration = "namedElements"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.architectury.loom") version "1.3.358" apply false
+    id("dev.architectury.loom") version "1.5.388" apply false
 }
 
 rootProject.name = "Pufferfish's Skills"
