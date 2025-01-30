9958d41afae1d0d7d1c4917571c7a43511348ef3
Pufferfish <puffish@puffish.net>
2025-01-30 18:26:50 +0100

    Move builtin registration to separate classes

---
+++ Common/src/main/java/net/puffish/skillsmod/experience/source/BuiltinExperienceSources.java
@@ -0,0 +1,27 @@
+package net.puffish.skillsmod.experience.source;
+
+import net.puffish.skillsmod.experience.source.builtin.CraftItemExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.DealDamageExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.EatFoodExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.FishItemExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.HealExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.IncreaseStatExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.KillEntityExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.MineBlockExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.SharedKillEntityExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.TakeDamageExperienceSource;
+
+public class BuiltinExperienceSources {
+	public static void register() {
+		CraftItemExperienceSource.register();
+		DealDamageExperienceSource.register();
+		EatFoodExperienceSource.register();
+		FishItemExperienceSource.register();
+		HealExperienceSource.register();
+		IncreaseStatExperienceSource.register();
+		KillEntityExperienceSource.register();
+		MineBlockExperienceSource.register();
+		SharedKillEntityExperienceSource.register();
+		TakeDamageExperienceSource.register();
+	}
+}
