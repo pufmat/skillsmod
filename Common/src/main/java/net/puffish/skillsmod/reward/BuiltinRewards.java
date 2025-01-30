8b66bd17a4c5470851e9fdcd4787b12cf57e1f9b
Pufferfish <puffish@puffish.net>
2025-01-30 18:26:50 +0100

    Move builtin registration to separate classes

---
+++ Common/src/main/java/net/puffish/skillsmod/reward/BuiltinRewards.java
@@ -0,0 +1,15 @@
+package net.puffish.skillsmod.reward;
+
+import net.puffish.skillsmod.reward.builtin.AttributeReward;
+import net.puffish.skillsmod.reward.builtin.CommandReward;
+import net.puffish.skillsmod.reward.builtin.ScoreboardReward;
+import net.puffish.skillsmod.reward.builtin.TagReward;
+
+public class BuiltinRewards {
+	public static void register() {
+		AttributeReward.register();
+		CommandReward.register();
+		ScoreboardReward.register();
+		TagReward.register();
+	}
+}
