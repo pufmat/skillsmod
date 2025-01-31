1bf67ce2491b21373b526f7525aa63f65dcb7295
Pufferfish <puffish@puffish.net>
2025-01-31 18:25:04 +0100

    Cleanup commands

--- Common/src/main/java/net/puffish/skillsmod/commands/OpenCommand.java
+++ Common/src/main/java/net/puffish/skillsmod/commands/OpenCommand.java
@@ -1,6 +1,8 @@
 package net.puffish.skillsmod.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
@@ -12,19 +14,21 @@ public class OpenCommand {
 		return CommandManager.literal("open")
 				.requires(source -> source.hasPermissionLevel(2))
 				.then(CommandManager.argument("players", EntityArgumentType.players())
-						.executes(context -> {
-							var players = EntityArgumentType.getPlayers(context, "players");
-
-							for (var player : players) {
-								SkillsAPI.openScreen(player);
-							}
-							CommandUtils.sendSuccess(
-									context,
-									players,
-									"open"
-							);
-							return players.size();
-						})
+						.executes(OpenCommand::open)
 				);
+	}
+
+	private static int open(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		var players = EntityArgumentType.getPlayers(context, "players");
+
+		for (var player : players) {
+			SkillsAPI.openScreen(player);
+		}
+		CommandUtils.sendSuccess(
+				context,
+				players,
+				"open"
+		);
+		return players.size();
 	}
 }
