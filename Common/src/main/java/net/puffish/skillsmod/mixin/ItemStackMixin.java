ff66119b8f5ef20c3c28741ab62e3721e3036f7c
Pufferfish <puffish@puffish.net>
2025-01-31 18:15:02 +0100

    Add break block experience source

--- Common/src/main/java/net/puffish/skillsmod/mixin/ItemStackMixin.java
+++ Common/src/main/java/net/puffish/skillsmod/mixin/ItemStackMixin.java
@@ -1,10 +1,13 @@
 package net.puffish.skillsmod.mixin;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.puffish.skillsmod.api.SkillsAPI;
+import net.puffish.skillsmod.experience.source.builtin.BreakBlockExperienceSource;
 import net.puffish.skillsmod.experience.source.builtin.CraftItemExperienceSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,6 +24,17 @@ public class ItemStackMixin {
 					serverPlayer,
 					CraftItemExperienceSource.class,
 					experienceSource -> experienceSource.getValue(serverPlayer, (ItemStack) (Object) this) * amount
+			);
+		}
+	}
+
+	@Inject(method = "postMine", at = @At("HEAD"))
+	private void injectAtPostMine(World world, BlockState state, BlockPos pos, PlayerEntity player, CallbackInfo ci) {
+		if (player instanceof ServerPlayerEntity serverPlayer) {
+			SkillsAPI.updateExperienceSources(
+					serverPlayer,
+					BreakBlockExperienceSource.class,
+					experienceSource -> experienceSource.getValue(serverPlayer, state, (ItemStack) (Object) this)
 			);
 		}
 	}
