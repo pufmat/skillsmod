e715a9ef9e97ce81e1debeb52e63d43b76fc8250
Pufferfish <puffish@puffish.net>
2025-01-31 17:29:55 +0100

    Add sources to points

---
+++ Common/src/main/java/net/puffish/skillsmod/util/PointSources.java
@@ -0,0 +1,10 @@
+package net.puffish.skillsmod.util;
+
+import net.minecraft.util.Identifier;
+import net.puffish.skillsmod.SkillsMod;
+
+public class PointSources {
+	public static final Identifier LEGACY = SkillsMod.createIdentifier("legacy");
+	public static final Identifier STARTING = SkillsMod.createIdentifier("starting");
+	public static final Identifier COMMANDS = SkillsMod.createIdentifier("commands");
+}
