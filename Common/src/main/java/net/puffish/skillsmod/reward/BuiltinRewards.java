a6b013461fee9c7366aa56df1cf79296fd0a2109
Pufferfish <puffish@puffish.net>
2025-02-01 13:00:56 +0100

    Add points reward

--- Common/src/main/java/net/puffish/skillsmod/reward/BuiltinRewards.java
+++ Common/src/main/java/net/puffish/skillsmod/reward/BuiltinRewards.java
@@ -2,6 +2,7 @@ package net.puffish.skillsmod.reward;
 
 import net.puffish.skillsmod.reward.builtin.AttributeReward;
 import net.puffish.skillsmod.reward.builtin.CommandReward;
+import net.puffish.skillsmod.reward.builtin.PointsReward;
 import net.puffish.skillsmod.reward.builtin.ScoreboardReward;
 import net.puffish.skillsmod.reward.builtin.TagReward;
 
@@ -9,6 +10,7 @@ public class BuiltinRewards {
 	public static void register() {
 		AttributeReward.register();
 		CommandReward.register();
+		PointsReward.register();
 		ScoreboardReward.register();
 		TagReward.register();
 	}
