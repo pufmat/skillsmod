ab163e5f4808cb0c812826831224b1b55fdd4a04
Pufferfish <puffish@puffish.net>
2025-02-05 12:09:18 +0100

    Move new points logic to server

--- Common/src/main/java/net/puffish/skillsmod/client/network/packets/in/PointsUpdateInPacket.java
+++ Common/src/main/java/net/puffish/skillsmod/client/network/packets/in/PointsUpdateInPacket.java
@@ -8,26 +8,22 @@ public class PointsUpdateInPacket implements InPacket {
 	private final Identifier categoryId;
 	private final int spentPoints;
 	private final int earnedPoints;
-	private final boolean announceNewPoints;
 
-	private PointsUpdateInPacket(Identifier categoryId, int spentPoints, int earnedPoints, boolean announceNewPoints) {
+	private PointsUpdateInPacket(Identifier categoryId, int spentPoints, int earnedPoints) {
 		this.categoryId = categoryId;
 		this.spentPoints = spentPoints;
 		this.earnedPoints = earnedPoints;
-		this.announceNewPoints = announceNewPoints;
 	}
 
 	public static PointsUpdateInPacket read(PacketByteBuf buf) {
 		var categoryId = buf.readIdentifier();
 		var spentPoints = buf.readInt();
 		var earnedPoints = buf.readInt();
-		var announceNewPoints = buf.readBoolean();
 
 		return new PointsUpdateInPacket(
 				categoryId,
 				spentPoints,
-				earnedPoints,
-				announceNewPoints
+				earnedPoints
 		);
 	}
 
@@ -41,9 +37,5 @@ public class PointsUpdateInPacket implements InPacket {
 
 	public int getEarnedPoints() {
 		return earnedPoints;
-	}
-
-	public boolean announceNewPoints() {
-		return announceNewPoints;
 	}
 }
