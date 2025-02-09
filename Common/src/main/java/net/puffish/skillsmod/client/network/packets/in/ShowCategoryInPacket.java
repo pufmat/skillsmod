e0c859a5169f1cc8759e27bf9e332592ec56f2e8
Pufferfish <puffish@puffish.net>
2025-02-09 18:19:00 +0100

    Workaround broken raw ids of effects

--- Common/src/main/java/net/puffish/skillsmod/client/network/packets/in/ShowCategoryInPacket.java
+++ Common/src/main/java/net/puffish/skillsmod/client/network/packets/in/ShowCategoryInPacket.java
@@ -131,8 +131,8 @@ public class ShowCategoryInPacket implements InPacket {
 		var type = buf.readEnumConstant(IconType.class);
 		return switch (type) {
 			case EFFECT -> {
-				var effect = buf.readRegistryValue(Registry.STATUS_EFFECT);
-				yield new ClientIconConfig.EffectIconConfig(effect);
+				var effect = buf.readIdentifier();
+				yield new ClientIconConfig.EffectIconConfig(Registry.STATUS_EFFECT.get(effect));
 			}
 			case ITEM -> {
 				var itemStack = buf.readItemStack();
