09849d119974dd9066fab1498b6fe2fcd51af567
Pufferfish <puffish@puffish.net>
2025-02-10 12:29:18 +0100

    Use point sources for points from experience

--- Common/src/main/java/net/puffish/skillsmod/util/PointSources.java
+++ Common/src/main/java/net/puffish/skillsmod/util/PointSources.java
@@ -7,4 +7,5 @@ public class PointSources {
 	public static final Identifier LEGACY = SkillsMod.createIdentifier("legacy");
 	public static final Identifier STARTING = SkillsMod.createIdentifier("starting");
 	public static final Identifier COMMANDS = SkillsMod.createIdentifier("commands");
+	public static final Identifier EXPERIENCE = SkillsMod.createIdentifier("experience");
 }
