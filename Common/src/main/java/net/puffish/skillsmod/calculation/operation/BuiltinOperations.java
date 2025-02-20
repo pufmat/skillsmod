ae220243d87e67133898f316360d6dbd656f2afa
Pufferfish <puffish@puffish.net>
2025-02-20 14:46:48 +0100

    Add test world operation

--- Common/src/main/java/net/puffish/skillsmod/calculation/operation/BuiltinOperations.java
+++ Common/src/main/java/net/puffish/skillsmod/calculation/operation/BuiltinOperations.java
@@ -15,6 +15,7 @@ import net.puffish.skillsmod.calculation.operation.builtin.StatTypeCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.StatValueOperation;
 import net.puffish.skillsmod.calculation.operation.builtin.SwitchOperation;
 import net.puffish.skillsmod.calculation.operation.builtin.TagCondition;
+import net.puffish.skillsmod.calculation.operation.builtin.WorldCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.legacy.LegacyBlockTagCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.legacy.LegacyEntityTypeTagCondition;
 import net.puffish.skillsmod.calculation.operation.builtin.legacy.LegacyItemTagCondition;
@@ -36,6 +37,7 @@ public class BuiltinOperations {
 		StatValueOperation.register();
 		SwitchOperation.register();
 		TagCondition.register();
+		WorldCondition.register();
 
 		LegacyBlockTagCondition.register();
 		LegacyEntityTypeTagCondition.register();
