da2b22ffc88d17dd5cece8755f2de172e8365016
Pufferfish <puffish@puffish.net>
2025-03-16 13:00:21 +0100

    Fix misleading error messages

--- Common/src/main/java/net/puffish/skillsmod/api/json/BuiltinJson.java
+++ Common/src/main/java/net/puffish/skillsmod/api/json/BuiltinJson.java
@@ -213,7 +213,7 @@ public final class BuiltinJson {
 						throw  new RuntimeException(e);
 					}
 				},
-				"state predicate"
+				"nbt predicate"
 		);
 	}
 
@@ -244,7 +244,7 @@ public final class BuiltinJson {
 						throw new RuntimeException(e);
 					}
 				},
-				"NBT"
+				"nbt"
 		);
 	}
 
@@ -252,7 +252,7 @@ public final class BuiltinJson {
 		try {
 			return Result.success(ComponentChanges.CODEC.parse(JsonOps.INSTANCE, element.getJson()).result().orElseThrow());
 		} catch (Exception e) {
-			return Result.failure(element.getPath().createProblem("Expected valid nbt"));
+			return Result.failure(element.getPath().createProblem("Expected components"));
 		}
 	}
 
