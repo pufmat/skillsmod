bdb99df4a59cd038b174f6a6931f12aa2812fff4
Pufferfish <puffish@puffish.net>
2025-05-11 17:35:38 +0200

    Add level limit option

--- Common/src/main/java/net/puffish/skillsmod/client/gui/SkillsScreen.java
+++ Common/src/main/java/net/puffish/skillsmod/client/gui/SkillsScreen.java
@@ -1046,6 +1046,7 @@ public class SkillsScreen extends Screen {
 						"tooltip",
 						"current_level",
 						activeCategoryData.getCurrentLevel()
+								+ (activeCategory.levelLimit() == Integer.MAX_VALUE ? "" : "/" + activeCategory.levelLimit())
 				).asOrderedText());
 				lines.add(SkillsMod.createTranslatable(
 						"tooltip",
