5063bb2691ac6308602c17bf122dcbbcb7e8f530
Pufferfish <puffish@puffish.net>
2025-05-20 14:27:36 +0200

    Add category description option

--- Common/src/main/java/net/puffish/skillsmod/client/gui/SkillsScreen.java
+++ Common/src/main/java/net/puffish/skillsmod/client/gui/SkillsScreen.java
@@ -848,6 +848,16 @@ public class SkillsScreen extends Screen {
 			if (isInsideTab(mouse, x)) {
 				var lines = new ArrayList<OrderedText>();
 				lines.add(categoryConfig.title().asOrderedText());
+				lines.addAll(Tooltip.wrapLines(client, Texts.setStyleIfAbsent(
+						categoryConfig.description().copy(),
+						Style.EMPTY.withFormatting(Formatting.GRAY)
+				)));
+				if (client.isShiftPressed()) {
+					lines.addAll(Tooltip.wrapLines(client, Texts.setStyleIfAbsent(
+							categoryConfig.extraDescription().copy(),
+							Style.EMPTY.withFormatting(Formatting.GRAY)
+					)));
+				}
 				if (client.options.advancedItemTooltips) {
 					lines.add(Text.literal(categoryConfig.id().toString()).formatted(Formatting.DARK_GRAY).asOrderedText());
 				}
