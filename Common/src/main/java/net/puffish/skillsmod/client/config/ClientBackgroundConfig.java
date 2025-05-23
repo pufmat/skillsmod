c872723a7f919761bdf73442c6fa46bb72934ad0
Pufferfish <puffish@puffish.net>
2025-05-23 15:58:08 +0200

    Add animated background support

--- Common/src/main/java/net/puffish/skillsmod/client/config/ClientBackgroundConfig.java
+++ Common/src/main/java/net/puffish/skillsmod/client/config/ClientBackgroundConfig.java
@@ -1,11 +1,99 @@
 package net.puffish.skillsmod.client.config;
 
+import com.mojang.blaze3d.platform.TextureUtil;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.resource.metadata.AnimationResourceMetadata;
+import net.minecraft.client.texture.AbstractTexture;
+import net.minecraft.client.texture.MissingSprite;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.TextureTickListener;
+import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
+import net.puffish.skillsmod.SkillsMod;
 import net.puffish.skillsmod.common.BackgroundPosition;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.io.IOException;
 
 public record ClientBackgroundConfig(
 		Identifier texture,
 		int width,
 		int height,
 		BackgroundPosition position
-) { }
+) {
+	public static ClientBackgroundConfig create(
+			Identifier textureId,
+			int width,
+			int height,
+			BackgroundPosition position
+	) {
+		var id = SkillsMod.createIdentifier(RandomStringUtils.random(16, "abcdefghijklmnopqrstuvwxyz0123456789"));
+		var texture = new ClientBackgroundTexture(textureId);
+		MinecraftClient.getInstance()
+				.getTextureManager()
+				.registerTexture(id, texture);
+
+		return new ClientBackgroundConfig(
+				id,
+				width,
+				height,
+				position
+		);
+	}
+
+	private static class ClientBackgroundTexture extends AbstractTexture implements TextureTickListener {
+		private final Identifier id;
+		private Sprite sprite;
+
+		public ClientBackgroundTexture(Identifier id) {
+			this.id = id;
+		}
+
+		@Override
+		public void load(ResourceManager manager) {
+			try {
+				var resource = manager.getResource(id);
+				var metadata = resource.getMetadata(AnimationResourceMetadata.READER);
+				if (metadata == null) {
+					metadata = AnimationResourceMetadata.EMPTY;
+				}
+				var image = NativeImage.read(resource.getInputStream());
+				var size = metadata.ensureImageSize(image.getWidth(), image.getHeight());
+				var info = new Sprite.Info(id, size.getFirst(), size.getSecond(), metadata);
+				sprite = new ClientBackgroundSprite(info, image.getWidth(), image.getHeight(), image);
+			} catch (IOException ignored) {
+				sprite = MissingSprite.getMissingSprite(null, 0, 16, 16, 0, 0);
+			}
+
+			RenderSystem.recordRenderCall(() -> {
+				bindTexture();
+				TextureUtil.prepareImage(this.getGlId(), 0, sprite.getWidth(), sprite.getHeight());
+				sprite.upload();
+			});
+		}
+
+		@Override
+		public void tick() {
+			bindTexture();
+			var animation = sprite.getAnimation();
+			if (animation != null) {
+				animation.tick();
+			}
+		}
+
+		@Override
+		public void close() {
+			sprite.close();
+
+			super.close();
+		}
+	}
+
+	private static class ClientBackgroundSprite extends Sprite {
+		public ClientBackgroundSprite(Info info, int width, int height, NativeImage image) {
+			super(null, info, 0, width, height, 0, 0, image);
+		}
+	}
+}
