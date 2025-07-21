445ce4330c4da68963eee710a1be2dc9999b035a
Pufferfish <puffish@puffish.net>
2025-07-21 16:37:09 +0200

    Add new points indicator

--- Common/src/main/java/net/puffish/skillsmod/client/gui/SkillsScreen.java
+++ Common/src/main/java/net/puffish/skillsmod/client/gui/SkillsScreen.java
@@ -57,6 +57,7 @@ public class SkillsScreen extends Screen {
 	private static final ButtonTextures PAGE_BACKWARD_TEXTURES = new ButtonTextures(
 			Identifier.ofVanilla("recipe_book/page_backward"), Identifier.ofVanilla("recipe_book/page_backward_highlighted")
 	);
+	private static final Identifier TRIAL_AVAILABLE_TEXTURE = Identifier.of("icon/trial_available");
 
 	private static final int TEXTURE_WIDTH = 256;
 	private static final int TEXTURE_HEIGHT = 256;
@@ -212,6 +213,7 @@ public class SkillsScreen extends Screen {
 					.max(Comparator.comparing(ClientCategoryData::getLastOpen));
 			resize();
 		}
+		optActiveCategoryData.ifPresent(ClientCategoryData::updateUnseenPoints);
 	}
 
 	private int getTabX(int i) {
@@ -815,6 +817,19 @@ public class SkillsScreen extends Screen {
 
 		textureRenderer.draw(context);
 		itemBatch.draw();
+
+		forEachVisibleTab((x, category) -> {
+			if (category.hasUnseenPoints()) {
+				context.drawGuiTexture(
+						RenderLayer::getGuiTextured,
+						TRIAL_AVAILABLE_TEXTURE,
+						x + 10,
+						FRAME_PADDING - 5,
+						8,
+						8
+				);
+			}
+		});
 	}
 
 	private void drawWindow(DrawContext context, double mouseX, double mouseY) {
