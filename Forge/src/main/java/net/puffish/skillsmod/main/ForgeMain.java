de2e6c20b9791ac0307861829e1eda8fc8f94c87
Pufferfish <puffish@puffish.net>
2025-09-15 12:30:36 +0200

    Improve game rules compatibility

--- Forge/src/main/java/net/puffish/skillsmod/main/ForgeMain.java
+++ Forge/src/main/java/net/puffish/skillsmod/main/ForgeMain.java
@@ -28,7 +28,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.server.ServerLifecycleHooks;
 import net.puffish.skillsmod.SkillsMod;
 import net.puffish.skillsmod.api.SkillsAPI;
-import net.puffish.skillsmod.mixin.GameRulesAccessor;
+import net.puffish.skillsmod.mixin.GameRulesInvoker;
 import net.puffish.skillsmod.network.InPacket;
 import net.puffish.skillsmod.network.OutPacket;
 import net.puffish.skillsmod.server.event.ServerEventListener;
@@ -117,8 +117,8 @@ public class ForgeMain {
 		}
 
 		@Override
-		public <T extends GameRules.Rule<T>> void registerGameRule(GameRules.Key<T> key, GameRules.Type<T> type) {
-			GameRulesAccessor.getRuleTypes().put(key, type);
+		public <T extends GameRules.Rule<T>> GameRules.Key<T> registerGameRule(String name, GameRules.Category category, GameRules.Type<T> type) {
+			return GameRulesInvoker.invokeRegister(name, category, type);
 		}
 
 		@Override
