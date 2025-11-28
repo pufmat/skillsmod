ab69992f0845ffefc493b017820268b44d412b14
Pufferfish <puffish@puffish.net>
2025-11-28 12:49:26 +0100

    Add anti farming to other experience sources

--- Common/src/main/java/net/puffish/skillsmod/experience/source/builtin/KillEntityExperienceSource.java
+++ Common/src/main/java/net/puffish/skillsmod/experience/source/builtin/KillEntityExperienceSource.java
@@ -41,7 +41,7 @@ import java.util.function.Function;
 
 public record KillEntityExperienceSource(
 		Calculation<Data> calculation,
-		Optional<AntiFarmingPerChunk> antiFarming,
+		Optional<AntiFarmingPerChunk> antiFarmingPerChunk,
 		TamedActivity tamedActivity
 ) implements ExperienceSource {
 
@@ -96,7 +96,8 @@ public record KillEntityExperienceSource(
 				.ifFailure(problems::add)
 				.getSuccess();
 
-		var optAntiFarming = rootObject.get("anti_farming")
+		var antiFarmingPerChunk = rootObject.get("anti_farming_per_chunk")
+				.orElse(LegacyUtils.wrapDeprecated(() -> rootObject.get("anti_farming"), 4, context))
 				.getSuccess() // ignore failure because this property is optional
 				.flatMap(element -> AntiFarmingPerChunk.parse(element, context)
 						.ifFailure(problems::add)
@@ -115,7 +116,7 @@ public record KillEntityExperienceSource(
 		if (problems.isEmpty()) {
 			return Result.success(new KillEntityExperienceSource(
 					optCalculation.orElseThrow(),
-					optAntiFarming,
+					antiFarmingPerChunk,
 					tamed
 			));
 		} else {
