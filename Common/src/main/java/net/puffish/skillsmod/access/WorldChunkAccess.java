e1629462aa256bc649725fda5abc41a98ac27205
Pufferfish <puffish@puffish.net>
2025-11-28 12:49:26 +0100

    Add anti farming to other experience sources

--- Common/src/main/java/net/puffish/skillsmod/access/WorldChunkAccess.java
+++ Common/src/main/java/net/puffish/skillsmod/access/WorldChunkAccess.java
@@ -3,5 +3,5 @@ package net.puffish.skillsmod.access;
 import net.puffish.skillsmod.experience.source.builtin.util.AntiFarmingPerChunk;
 
 public interface WorldChunkAccess {
-	AntiFarmingPerChunk.Data getAntiFarmingData();
+	AntiFarmingPerChunk.State getAntiFarmingPerChunkState();
 }
