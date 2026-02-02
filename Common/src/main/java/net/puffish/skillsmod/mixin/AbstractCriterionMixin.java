da7276797d1ae01fc3d83a51ae8bab06b6ac5899
Pufferfish <puffish@puffish.net>
2026-02-02 16:10:36 +0100

    Add criterion experience source

---
+++ Common/src/main/java/net/puffish/skillsmod/mixin/AbstractCriterionMixin.java
@@ -0,0 +1,37 @@
+package net.puffish.skillsmod.mixin;
+
+import net.minecraft.advancement.criterion.AbstractCriterion;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.puffish.skillsmod.api.SkillsAPI;
+import net.puffish.skillsmod.experience.source.builtin.CriterionExperienceSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Predicate;
+
+@Mixin(AbstractCriterion.class)
+public class AbstractCriterionMixin {
+
+	@Inject(method = "trigger", at = @At("HEAD"))
+	private void injectAtTrigger(ServerPlayerEntity player, Predicate<Object> predicate, CallbackInfo ci) {
+		SkillsAPI.updateExperienceSources(
+				player,
+				CriterionExperienceSource.class,
+				es -> {
+					var conditions = es.criterion().getConditions();
+					if (conditions != null && this.equals(Criteria.getById(conditions.getId()))) {
+						if (predicate.test(es.criterion().getConditions())) {
+							return (int) Math.round(es.calculation().evaluate(
+									new CriterionExperienceSource.Data(player)
+							));
+						}
+					}
+					return 0;
+				}
+		);
+	}
+
+}
