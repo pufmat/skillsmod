5e2c68ca92778f9f6697ead2d753e581729f1957
Pufferfish <puffish@puffish.net>
2026-02-12 15:21:24 +0100

    Add category level text

--- Common/src/main/java/net/puffish/skillsmod/client/gui/SkillsScreen.java
+++ Common/src/main/java/net/puffish/skillsmod/client/gui/SkillsScreen.java
@@ -1211,6 +1211,17 @@ public class SkillsScreen extends Screen {
 				).asOrderedText());
 				context.drawTooltip(lines, mouseX, mouseY);
 			}
+
+			if (activeCategoryData.getCurrentLevel() > 0) {
+				tmpText = Text.literal("" + activeCategoryData.getCurrentLevel());
+				tmpX += (182 - this.textRenderer.getWidth(tmpText)) / 2;
+				tmpY -= 1;
+				context.drawText(this.textRenderer, tmpText, tmpX - 1, tmpY, pointsStrokeColor, false);
+				context.drawText(this.textRenderer, tmpText, tmpX, tmpY - 1, pointsStrokeColor, false);
+				context.drawText(this.textRenderer, tmpText, tmpX + 1, tmpY, pointsStrokeColor, false);
+				context.drawText(this.textRenderer, tmpText, tmpX, tmpY + 1, pointsStrokeColor, false);
+				context.drawText(this.textRenderer, tmpText, tmpX, tmpY, pointsFillColor, false);
+			}
 		}
 	}
 
