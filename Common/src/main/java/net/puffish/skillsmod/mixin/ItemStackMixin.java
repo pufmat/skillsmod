cdcd5a1c230bb0971b3bf1a2077db55df62e3fcd
Pufferfish <puffish@puffish.net>
2026-02-22 21:07:30 +0100

    Only count crafting in Craft Item Experience Source

--- Common/src/main/java/net/puffish/skillsmod/mixin/ItemStackMixin.java
+++ Common/src/main/java/net/puffish/skillsmod/mixin/ItemStackMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.puffish.skillsmod.api.SkillsAPI;
 import net.puffish.skillsmod.experience.source.builtin.BreakBlockExperienceSource;
-import net.puffish.skillsmod.experience.source.builtin.CraftItemExperienceSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,19 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-
-	@Inject(method = "onCraftByPlayer", at = @At("HEAD"))
-	private void injectAtOnCraftByPlayer(PlayerEntity player, int amount, CallbackInfo ci) {
-		if (player instanceof ServerPlayerEntity serverPlayer) {
-			SkillsAPI.updateExperienceSources(
-					serverPlayer,
-					CraftItemExperienceSource.class,
-					es -> (int) Math.round(es.calculation().evaluate(
-							new CraftItemExperienceSource.Data(serverPlayer, (ItemStack) (Object) this)
-					) * amount)
-			);
-		}
-	}
 
 	@Inject(method = "postMine", at = @At("HEAD"))
 	private void injectAtPostMine(World world, BlockState state, BlockPos pos, PlayerEntity player, CallbackInfo ci) {
