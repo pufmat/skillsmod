16d1a8c2b9f6e60810e4601def4eb6fbd8b7578b
Pufferfish <puffish@puffish.net>
2026-07-09 14:36:18 +0200

    Dispose unused textures

--- Common/src/main/java/net/puffish/skillsmod/client/config/ClientBackgroundConfig.java
+++ Common/src/main/java/net/puffish/skillsmod/client/config/ClientBackgroundConfig.java
@@ -90,4 +90,12 @@ public record ClientBackgroundConfig(
 			super.close();
 		}
 	}
+
+	public void dispose() {
+		MinecraftClient.getInstance().execute(() -> {
+			MinecraftClient.getInstance()
+					.getTextureManager()
+					.destroyTexture(this.texture);
+		});
+	}
 }
