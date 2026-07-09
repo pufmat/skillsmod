357fe7e2711c9ee187a475c3d62c53c17f964084
Pufferfish <puffish@puffish.net>
2026-07-09 14:40:17 +0200

    Prevent negative experience

--- Common/src/main/java/net/puffish/skillsmod/SkillsMod.java
+++ Common/src/main/java/net/puffish/skillsmod/SkillsMod.java
@@ -407,6 +407,10 @@ public class SkillsMod {
 	}
 
 	public void setExperience(ServerPlayerEntity player, CategoryConfig category, ExperienceConfig experience, CategoryData categoryData, int amount) {
+		if (amount < 0) {
+			amount = 0;
+		}
+
 		var curve = experience.curve();
 		var level = curve.getProgress(amount).currentLevel();
 		var levelLimit = curve.getLevelLimit();
