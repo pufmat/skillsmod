13df2e50b0069490acae74fc37906dc86b16a7e1
Pufferfish <puffish@puffish.net>
2026-07-09 15:22:31 +0200

    Improve block mine and break compatibility

--- Fabric/src/main/java/net/puffish/skillsmod/main/FabricMain.java
+++ Fabric/src/main/java/net/puffish/skillsmod/main/FabricMain.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.FakePlayer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
@@ -18,6 +19,9 @@ import net.minecraft.registry.Registry;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.puffish.skillsmod.SkillsMod;
+import net.puffish.skillsmod.api.SkillsAPI;
+import net.puffish.skillsmod.experience.source.builtin.BreakBlockExperienceSource;
+import net.puffish.skillsmod.experience.source.builtin.MineBlockExperienceSource;
 import net.puffish.skillsmod.network.OutPacket;
 import net.puffish.skillsmod.server.event.ServerEventListener;
 import net.puffish.skillsmod.server.event.ServerEventReceiver;
@@ -42,6 +46,28 @@ public class FabricMain implements ModInitializer {
 				new ServerPacketSenderImpl(),
 				new ServerPlatformImpl()
 		);
+
+		PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, blockEntity) -> {
+			if (player instanceof ServerPlayerEntity serverPlayer) {
+				var stack = serverPlayer.getMainHandStack();
+				SkillsAPI.updateExperienceSources(
+						serverPlayer,
+						BreakBlockExperienceSource.class,
+						experienceSource -> (int) Math.round(experienceSource.calculation().evaluate(
+								new BreakBlockExperienceSource.Data(serverPlayer, state, stack)
+						))
+				);
+				if (serverPlayer.canHarvest(state)) {
+					SkillsAPI.updateExperienceSources(
+							serverPlayer,
+							MineBlockExperienceSource.class,
+							es -> (int) Math.round(es.calculation().evaluate(
+									new MineBlockExperienceSource.Data(serverPlayer, state, stack)
+							))
+					);
+				}
+			}
+		});
 	}
 
 	private class ServerRegistrarImpl implements ServerRegistrar {
