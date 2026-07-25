2b5c9a10ce4ed50f919a523b314ee8d03ca75507
Pufferfish <puffish@puffish.net>
2026-07-25 14:49:05 +0200

    Add skill unlocked criterion

---
+++ Common/src/main/java/net/puffish/skillsmod/mixin/CriteriaInvoker.java
@@ -0,0 +1,14 @@
+package net.puffish.skillsmod.mixin;
+
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.advancement.criterion.Criterion;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Criteria.class)
+public interface CriteriaInvoker {
+	@Invoker("register")
+	static <T extends Criterion<?>> T register(T object) {
+		throw new AssertionError();
+	}
+}
