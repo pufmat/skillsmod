b3c4b836280f269f8fb8084c1852ec7622db226c
Pufferfish <puffish@puffish.net>
2026-07-25 14:49:05 +0200

    Add skill unlocked criterion

---
+++ Common/src/main/java/net/puffish/skillsmod/server/setup/SkillsTriggers.java
@@ -0,0 +1,53 @@
+package net.puffish.skillsmod.server.setup;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.PrimitiveCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.advancements.criterion.ContextAwarePredicate;
+import net.minecraft.advancements.criterion.EntityPredicate;
+import net.minecraft.advancements.criterion.SimpleCriterionTrigger;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.level.ServerPlayer;
+import net.puffish.skillsmod.SkillsMod;
+import net.puffish.skillsmod.api.SkillsAPI;
+
+import java.util.Optional;
+
+public class SkillsTriggers {
+
+	public static SkillUnlockedTrigger SKILL_UNLOCKED = new SkillUnlockedTrigger();
+
+	public static void register(ServerRegistrar registrar) {
+		SkillsAPI.registerSkillUnlockEvent((player, categoryId, skillId) -> {
+			SKILL_UNLOCKED.trigger(player, categoryId, skillId);
+		});
+		registrar.register(BuiltInRegistries.TRIGGER_TYPES, SkillsMod.createIdentifier("skill_unlocked"), SKILL_UNLOCKED);
+	}
+
+	public static class SkillUnlockedTrigger extends SimpleCriterionTrigger<SkillUnlockedTrigger.Conditions> {
+		public void trigger(ServerPlayer player, Identifier categoryId, String skillId) {
+			this.trigger(player, conditions -> conditions.matches(categoryId, skillId));
+		}
+
+		public Codec<Conditions> codec() {
+			return Conditions.CODEC;
+		}
+
+		public record Conditions(
+				Optional<ContextAwarePredicate> player,
+				Identifier categoryId,
+				String skillId
+		) implements SimpleCriterionTrigger.SimpleInstance {
+			public static final Codec<Conditions> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
+					EntityPredicate.ADVANCEMENT_CODEC.optionalFieldOf("player").forGetter(Conditions::player),
+					Identifier.CODEC.fieldOf("category").forGetter(Conditions::categoryId),
+					PrimitiveCodec.STRING.fieldOf("skill").forGetter(Conditions::skillId)
+			).apply(instance, Conditions::new));
+
+			public boolean matches(Identifier categoryId, String skillId) {
+				return this.categoryId.equals(categoryId) && this.skillId.equals(skillId);
+			}
+		}
+	}
+}
