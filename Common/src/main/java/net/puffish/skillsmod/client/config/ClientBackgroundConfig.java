dcb18274c1b02fff815ee5b8adc93d5d1f319f93
Pufferfish <puffish@puffish.net>
2026-07-28 23:10:41 +0200

    Fix background texture crash

--- Common/src/main/java/net/puffish/skillsmod/client/config/ClientBackgroundConfig.java
+++ Common/src/main/java/net/puffish/skillsmod/client/config/ClientBackgroundConfig.java
@@ -72,11 +72,16 @@ public record ClientBackgroundConfig(
 					})
 					.orElseGet(() -> MissingSprite.getMissingSprite(null, 0, 16, 16, 0, 0));
 
-			RenderSystem.recordRenderCall(() -> {
+			RenderSystem.recordRenderCall(this::setup);
+		}
+
+		private void setup() {
+			// close may be called before setup is queued
+			if (sprite != null) {
 				bindTexture();
 				TextureUtil.prepareImage(this.getGlId(), 0, sprite.getWidth(), sprite.getHeight());
 				sprite.upload();
-			});
+			}
 		}
 
 		@Override
@@ -91,6 +96,7 @@ public record ClientBackgroundConfig(
 		@Override
 		public void close() {
 			sprite.close();
+			sprite = null;
 
 			super.close();
 		}
