70b0a2fb0685f917ecd25acb58bf852cbd88513b
Pufferfish <puffish@puffish.net>
2026-07-31 14:12:44 +0200

    Add option to reset experience on death

--- Common/src/main/java/net/puffish/skillsmod/SkillsMod.java
+++ Common/src/main/java/net/puffish/skillsmod/SkillsMod.java
@@ -307,8 +307,19 @@ public class SkillsMod {
 	}
 
 	public void onPlayerDeath(ServerPlayer player) {
+		if (platform.isFakePlayer(player)) {
+			return;
+		}
 		var playerData = getPlayerData(player);
 		for (var category : getAllCategories()) {
+			var categoryData = playerData.getOrCreateCategoryData(category);
+			category.experience().ifPresent(experience -> {
+				if (experience.resetOnDeath()) {
+					var amount = categoryData.getExperience();
+					amount -= experience.curve().getProgress(amount).currentExperience();
+					setExperience(player, category, experience, categoryData, amount);
+				}
+			});
 			if (category.general().eraseOnDeath()) {
 				eraseCategory(player, playerData, category);
 			}
