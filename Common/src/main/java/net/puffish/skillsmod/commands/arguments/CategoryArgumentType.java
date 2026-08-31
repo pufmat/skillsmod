2df9f181894fa3a59b78574d9ddf6fbc570b358d
Pufferfish <puffish@puffish.net>
2026-08-31 13:12:31 +0200

    Add level command

--- Common/src/main/java/net/puffish/skillsmod/commands/arguments/CategoryArgumentType.java
+++ Common/src/main/java/net/puffish/skillsmod/commands/arguments/CategoryArgumentType.java
@@ -16,6 +16,7 @@ import net.minecraft.util.Identifier;
 import net.puffish.skillsmod.SkillsMod;
 import net.puffish.skillsmod.api.Category;
 import net.puffish.skillsmod.api.SkillsAPI;
+import net.puffish.skillsmod.util.CategoryFilter;
 import net.puffish.skillsmod.util.CommandUtils;
 
 import java.util.concurrent.CompletableFuture;
@@ -26,18 +27,30 @@ public class CategoryArgumentType implements ArgumentType<Identifier> {
 			id -> SkillsMod.createTranslatable("command", "no_such_category", id)
 	);
 
-	private final boolean onlyWithExperience;
+	private static final DynamicCommandExceptionType NO_EXPERIENCE = new DynamicCommandExceptionType(
+			id -> SkillsMod.createTranslatable("command", "no_experience", id)
+	);
 
-	public CategoryArgumentType(boolean onlyWithExperience) {
-		this.onlyWithExperience = onlyWithExperience;
+	private static final DynamicCommandExceptionType NO_LEVEL = new DynamicCommandExceptionType(
+			id -> SkillsMod.createTranslatable("command", "no_level", id)
+	);
+
+	private final CategoryFilter filter;
+
+	public CategoryArgumentType(CategoryFilter filter) {
+		this.filter = filter;
 	}
 
 	public static CategoryArgumentType category() {
-		return new CategoryArgumentType(false);
+		return new CategoryArgumentType(CategoryFilter.ALL);
 	}
 
 	public static CategoryArgumentType categoryOnlyWithExperience() {
-		return new CategoryArgumentType(true);
+		return new CategoryArgumentType(CategoryFilter.WITH_EXPERIENCE);
+	}
+
+	public static CategoryArgumentType categoryOnlyWithLevel() {
+		return new CategoryArgumentType(CategoryFilter.WITH_LEVEL);
 	}
 
 	public static Category getCategory(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
@@ -48,9 +61,22 @@ public class CategoryArgumentType implements ArgumentType<Identifier> {
 
 	public static Category getCategoryOnlyWithExperience(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
 		var categoryId = SkillsMod.convertIdentifier(context.getArgument(name, Identifier.class));
-		return SkillsAPI.getCategory(categoryId)
-				.filter(category -> category.getExperience().isPresent())
+		var category = SkillsAPI.getCategory(categoryId)
 				.orElseThrow(() -> NO_SUCH_CATEGORY.create(categoryId));
+		if (category.getExperience().isEmpty()) {
+			throw NO_EXPERIENCE.create(categoryId);
+		}
+		return category;
+	}
+
+	public static Category getCategoryOnlyWithLevel(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
+		var categoryId = SkillsMod.convertIdentifier(context.getArgument(name, Identifier.class));
+		var category = SkillsAPI.getCategory(categoryId)
+				.orElseThrow(() -> NO_SUCH_CATEGORY.create(categoryId));
+		if (category.getExperience().isEmpty() && category.getExchange().isEmpty()) {
+			throw NO_LEVEL.create(categoryId);
+		}
+		return category;
 	}
 
 	@Override
@@ -62,7 +88,7 @@ public class CategoryArgumentType implements ArgumentType<Identifier> {
 	public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
 		var source = context.getSource();
 		if (source instanceof ServerCommandSource) {
-			CommandUtils.suggestIdentifiers(SkillsMod.getInstance().getCategories(onlyWithExperience), builder);
+			CommandUtils.suggestIdentifiers(SkillsMod.getInstance().getCategories(filter), builder);
 			return builder.buildFuture();
 		} else if (source instanceof CommandSource commandSource) {
 			return commandSource.getCompletions(context);
@@ -74,17 +100,17 @@ public class CategoryArgumentType implements ArgumentType<Identifier> {
 
 		@Override
 		public void toPacket(CategoryArgumentType argumentType, PacketByteBuf buf) {
-			buf.writeBoolean(argumentType.onlyWithExperience);
+			buf.writeEnumConstant(argumentType.filter);
 		}
 
 		@Override
 		public CategoryArgumentType fromPacket(PacketByteBuf buf) {
-			return new CategoryArgumentType(buf.readBoolean());
+			return new CategoryArgumentType(buf.readEnumConstant(CategoryFilter.class));
 		}
 
 		@Override
 		public void toJson(CategoryArgumentType argumentType, JsonObject jsonObject) {
-			jsonObject.addProperty("only_with_experience", argumentType.onlyWithExperience);
+			jsonObject.addProperty("filter", argumentType.filter.name().toLowerCase());
 		}
 	}
 }
