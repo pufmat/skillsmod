33bd07c8170e1639f106e97d64f0cab3b8dadcc8
Pufferfish <puffish@puffish.net>
2026-08-31 13:56:39 +0200

    Add new point event

--- Common/src/main/java/net/puffish/skillsmod/SkillsMod.java
+++ Common/src/main/java/net/puffish/skillsmod/SkillsMod.java
@@ -101,6 +101,9 @@ public class SkillsMod {
 	public static final Event<Events.SkillLock> SKILL_LOCK = Event.create(
 			c -> (player, categoryId, skillId) -> c.forEach(e -> e.onSkillLock(player, categoryId, skillId))
 	);
+	public static final Event<Events.NewPoint> NEW_POINT = Event.create(
+			c -> (player, categoryId) -> c.forEach(e -> e.onNewPoint(player, categoryId))
+	);
 
 	private static SkillsMod instance;
 
@@ -786,6 +789,7 @@ public class SkillsMod {
 			var pointsLeft = categoryData.getPointsLeft(category);
 			runnable.run();
 			if (categoryData.getPointsLeft(category) > pointsLeft) {
+				NEW_POINT.invoker().onNewPoint(player, category.id());
 				if (player.getEntityWorld().getGameRules().getValue(SkillsGameRules.ANNOUNCE_NEW_POINTS)) {
 					packetSender.send(player, new NewPointOutPacket(category.id()));
 				}
