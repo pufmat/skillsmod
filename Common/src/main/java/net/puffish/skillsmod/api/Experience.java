b10138dfd1c68a761797eedbde4e05257d1ad0ed
Pufferfish <puffish@puffish.net>
2026-08-31 13:12:31 +0200

    Add level command

--- Common/src/main/java/net/puffish/skillsmod/api/Experience.java
+++ Common/src/main/java/net/puffish/skillsmod/api/Experience.java
@@ -12,6 +12,12 @@ public interface Experience {
 	/// Returns the current level based on the total experience.
 	int getLevel(ServerPlayerEntity player);
 
+	/// Modifies total experience to set given level. Current experience is scaled.
+	void setLevel(ServerPlayerEntity player, int level);
+
+	/// Modifies total experience to add given count of levels. Current experience is scaled.
+	void addLevel(ServerPlayerEntity player, int count);
+
 	/// Returns the current experience based on the total experience.
 	int getCurrent(ServerPlayerEntity player);
 
